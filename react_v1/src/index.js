993b80502fd9ce0570a717583d07c7022a798f77
Mangi Lal <mangilal1036@gmail.com>
2023-10-08 02:49:04 +0530

    Netflix challenge

--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -6,8 +6,21 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-ReactDom.render(<h1> Mangi Lal</h1>,
-    document.getElementById("root")); // "root" is in index.html...
+
+ReactDom.render(
+  <>
+    <h1> Netflix</h1>
+    <p> List of 5 Best Series</p>
+    <ol>
+      <li>Dark</li>
+      <li>Extra curricular</li>
+      <li>My Holo love</li>
+      <li>My first-2 love</li>
+      <li>Mr Robot</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+); // "root" is in index.html...
 
 {/* <h1> Hello World!!!</h1> // this is not html. this is JSX expression in react */}
 
