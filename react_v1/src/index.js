8f7a932c552ce1a709466d1a1f288466aa8de5e7
Mangi Lal <mangilal1036@gmail.com>
2023-10-08 20:12:29 +0530

    Inline CSS

--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -6,9 +6,18 @@ const img2 = "https://picsum.photos/250/300";
 const img3 = "https://picsum.photos/300/300";
 const portfolio = 'https://mangilal-portfolio.netlify.app/';
 
+const heading = {
+  color: '#fa9191',
+  textTransform: 'capitalize',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  textShadow: '0px 2px 4px #ffe9c5',
+  margin: '50px 0',
+  fontFamily: '"Josefin Slab", serif'
+}
 ReactDOM.render(
   <>
-    <h1 className="heading">Hello, My Name is Mangi Lal</h1>
+    <h1 style={heading} >Hello, My Name is Mangi Lal</h1>
     
     <div className="img_div">
     <img src={img1} alt="randomimg"/>
