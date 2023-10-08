0fbb1c1e3ce4dc6a1180c8861ea1ca58425352f6
Mangi Lal <mangilal1036@gmail.com>
2023-10-08 22:23:15 +0530

    Greeting Website Challenge

--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -1,31 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css'; 
-const img1 = "https://picsum.photos/200/300";
-const img2 = "https://picsum.photos/250/300";
-const img3 = "https://picsum.photos/300/300";
-const portfolio = 'https://mangilal-portfolio.netlify.app/';
-
-const heading = {
-  color: '#fa9191',
-  textTransform: 'capitalize',
-  textAlign: 'center',
-  fontWeight: 'bold',
-  textShadow: '0px 2px 4px #ffe9c5',
-  margin: '50px 0',
-  fontFamily: '"Josefin Slab", serif'
+//new Date(year, month, day, hours, minute, second...)
+let currDate = new Date();
+let currTime = currDate.getHours();
+let greeting = '';
+const cssStyle = {};
+// conditional statements
+if(currTime>=1 && currTime<12){
+  greeting = 'Good Morning';
+  cssStyle.color = 'green'; // dynamically adding color to cssStyle object.
+}else if(currTime>=12 && currTime<19){
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'orange';
+}else{
+  greeting = 'Good Night';
+  cssStyle.color = 'gray';
 }
 ReactDOM.render(
   <>
-    <h1 style={heading} >Hello, My Name is Mangi Lal</h1>
-    
-    <div className="img_div">
-    <img src={img1} alt="randomimg"/>
-    <img src={img2} alt="randomimg"/>
-    <a href={portfolio} target="_blank">
-      <img src={img3} alt="randomimg"/> 
-    </a>
-    </div>
+  <div>
+    <h1> Hello Sir, <span style={cssStyle}> {greeting} </span> </h1>
+  </div>
   </>
   ,document.getElementById("root")
 );
