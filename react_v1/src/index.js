f18358a7bd32fb88b96ed4ff7f4cc31a58e4206d
Mangi Lal <mangilal1036@gmail.com>
2023-10-08 18:52:30 +0530

    CSS styling in react

--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import './index.css'; 
 const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/250/300";
-const img3 = "https://picsum.photos/400/300";
+const img3 = "https://picsum.photos/300/300";
 const portfolio = 'https://mangilal-portfolio.netlify.app/';
 
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">Hello, My Name is Mangi Lal</h1>
+    <h1 className="heading">Hello, My Name is Mangi Lal</h1>
+    
+    <div className="img_div">
     <img src={img1} alt="randomimg"/>
     <img src={img2} alt="randomimg"/>
     <a href={portfolio} target="_blank">
       <img src={img3} alt="randomimg"/> 
     </a>
+    </div>
   </>
-  
   ,document.getElementById("root")
 );
