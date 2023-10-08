10eabd84d5dfb6c0b241dd4e52149caff14acdfe
Mangi Lal <mangilal1036@gmail.com>
2023-10-08 12:55:07 +0530

    Expression in React

--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -6,20 +6,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-
+const fname = 'Mangi';
 ReactDom.render(
   <>
-    <h1> Netflix</h1>
-    <p> List of 5 Best Series</p>
-    <ol>
-      <li>Dark</li>
-      <li>Extra curricular</li>
-      <li>My Holo love</li>
-      <li>My first-2 love</li>
-      <li>Mr Robot</li>
-    </ol>
-  </>,
-  document.getElementById("root")
+    <h1>My First name is {fname}</h1>
+    <p>My lucky Number is {9 * 4}</p>
+    <p>Random Number {Math.random()}</p>
+  </>
+  
+  ,document.getElementById("root")
 ); // "root" is in index.html...
 
 {/* <h1> Hello World!!!</h1> // this is not html. this is JSX expression in react */}
