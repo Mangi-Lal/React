f05c35b552781abc6713ffffce011623f288ee9b
Mangi Lal <mangilal1036@gmail.com>
2023-10-10 23:11:16 +0530

    import  and export

--- react_v1/src/App.jsx
+++ react_v1/src/App.jsx
@@ -1,1 +1,14 @@
 //App.jsx
+const youtuber = 'Carryminati';
+const favprog = 'CPP';
+
+function myName(){
+    let Name = 'Mangi Lal';
+    return (Name);
+}
+function myRollNo(){
+    let rollno = '201060';
+    return (rollno);
+}
+export default youtuber;
+export {favprog,myName,myRollNo};
--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -1,10 +1,17 @@
 //index.js
 import React from "react";
 import ReactDOM from "react-dom";
-
+// import youtuber,{favprog,myName,myRollNo} from "./App";
+import * as details from './App';
 ReactDOM.render(
   <>
-  <h1>Hellooooooo</h1>
+  <ol>
+    <li>Helloooooo</li>
+    <li>{details.default}</li>
+    <li>{details.favprog}</li>
+    <li>{details.myName()}</li>
+    <li>{details.myRollNo()}</li>
+  </ol>
   </>,
   document.getElementById('root')
 );
