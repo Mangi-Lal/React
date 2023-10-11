588a4c80e3a62b24b20ee6beb70f9f388eb095d9
Mangi Lal <mangilal1036@gmail.com>
2023-10-11 19:17:38 +0530

    props in react

--- react_v1/src/App.jsx
+++ react_v1/src/App.jsx
@@ -1,16 +1,1 @@
 //App.jsx
-import React from "react";
-import {add,sub,mul,div} from './Calculator';
-
-let App = () => {
-    return(
-        <>
-            <li>Sum of two numbers is: {add(30,6)}</li>
-            <li>Substraction of two numbers is: {sub(30,6)}</li>
-            <li>Multiplication of two numbers is: {mul(30,6)}</li>
-            <li>Division of two numbers is: {div(30,7)}</li>
-        </>
-    );
-}
-
-export default App;
--- react_v1/src/index.js
+++ react_v1/src/index.js
@@ -1,9 +1,28 @@
 //index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Card from "./Cards";
 
 ReactDOM.render(
-  <App/>,
+  <>
+  <Card
+  imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+  title="A Netflix Original Series"
+  sname = "DARK"
+  link = "https://www.netflix.com/in/title/80100172"
+  />
+  <Card
+  imgsrc="https://www.hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpg"
+  title="A Netflix Original Series"
+  sname = "Extra curricular"
+  link = "https://www.netflix.com/in/title/80990668"
+  />
+  <Card
+  imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
+  title="A Netflix Original Series"
+  sname = "Stranger Things"
+  link = "https://www.netflix.com/in/title/80057281"
+  />
+  </>,
   document.getElementById('root')
 );
