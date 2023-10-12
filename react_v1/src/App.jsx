840306dee359b6d42408cd643e68cb062c7ed0d4
Mangi Lal <mangilal1036@gmail.com>
2023-10-12 19:32:53 +0530

    Ternary operator in React

--- react_v1/src/App.jsx
+++ react_v1/src/App.jsx
@@ -3,21 +3,13 @@ import React from "react";
 import Netflix from "./Netflix";
 import Amazon from "./Amazon";
 
-const favseries = 'amazon';
-let FavS = () =>{
-    if(favseries === 'netflix'){
-        return <Netflix/>;
-    }
-    else {
-        return <Amazon/>;
-    }
-}
+const favseries = 'netflix';
 
 const App = () =>{
     return(
     <>
         <h1 className="heading_style"> Top Web series in 2020 </h1>
-        <FavS/>
+        {favseries === 'netflix'?<Netflix/>:<Amazon/>}
     </>
     );
 }
