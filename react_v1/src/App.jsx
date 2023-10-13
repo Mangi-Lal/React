a3c5c4038c75dbfc1296ac5b7114c11ce5b3d10f
Mangi Lal <mangilal1036@gmail.com>
2023-10-13 16:38:34 +0530

    Handling Complex Input

--- react_v1/src/App.jsx
+++ react_v1/src/App.jsx
@@ -2,39 +2,55 @@
 import React, { useState } from "react";
 
 const App = () =>{
-    const [firstData,setfData] = useState();
-    const [FirstName,setfName] = useState();
-
-    const [lastData,setlData] = useState();
-    const [LastName,setlName] = useState();
-    const inputValuetwo = (event) =>{
-        setlData(event.target.value);
-    }
+    const [fullName,setfName] = useState({
+        fname:"",
+        lname:""
+    });
+    
+    
 
     const inputValue = (event) =>{
-        setfData(event.target.value);
+        // setfName(event.target.value);
+        const value = event.target.value;
+        const name = event.target.name;
+
+        setfName((preValue) =>{
+            if(name === "fName"){
+                return {
+                    fname: value,
+                    lname: preValue.lname,
+                }
+            }
+            else if(name === "lName"){
+                return {
+                    fname: preValue.fname,
+                    lname: value,
+                }
+            }
+        })
     }
     const onSubmit = (event) =>{
         //It is used to prevent default form behaviour
         event.preventDefault();
-        setfName(firstData);
-        setlName(lastData);
+        alert('Form submitted');
     }
     return(
     <>
     <div className="main_div">
         <form onSubmit={onSubmit}>
             <div>
-                <h1> Hello {FirstName} {LastName} </h1>
+                <h1> Hello {fullName.fname} {fullName.lname} </h1>
                 <input type="text" 
-                    placeholder="Enter Your Name" 
+                    placeholder="Enter Your Name"
+                    name="fName" 
                     onChange={inputValue}
-                    value={firstData}
+                    value={fullName.fname}
                 />
                 <input type="text" 
-                    placeholder="Enter Your Last Name" 
-                    onChange={inputValuetwo}
-                    value={lastData}
+                    placeholder="Enter Your Last Name"
+                    name="lName" 
+                    onChange={inputValue}
+                    value={fullName.lname}
                 />
                 <button type="submit"> Submit</button>
             </div>
