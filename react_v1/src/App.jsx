3009d459e6a92a85f6f9886e9592b55c1bbf9e4e
Mangi Lal <mangilal1036@gmail.com>
2023-10-13 16:59:20 +0530

    Login form challenge

--- react_v1/src/App.jsx
+++ react_v1/src/App.jsx
@@ -2,29 +2,50 @@
 import React, { useState } from "react";
 
 const App = () =>{
+    //Here we are passing object to useState()..
     const [fullName,setfName] = useState({
         fname:"",
-        lname:""
+        lname:"",
+        email:"",
+        phone:"",
     });
-    
-    
-
     const inputValue = (event) =>{
         // setfName(event.target.value);
-        const value = event.target.value;
-        const name = event.target.name;
 
+        const {value,name} = event.target;
         setfName((preValue) =>{
             if(name === "fName"){
+                //As we know initial value of useState(),we passed, is an object.
+                //so we are required to return an object.
                 return {
                     fname: value,
                     lname: preValue.lname,
+                    email: preValue.email,
+                    phone: preValue.phone,
                 }
             }
             else if(name === "lName"){
                 return {
                     fname: preValue.fname,
                     lname: value,
+                    email: preValue.email,
+                    phone: preValue.phone,
+                }
+            }
+            else if(name === "email"){
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone: preValue.phone,
+                }
+            }
+            else if(name === "phone"){
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: value,
                 }
             }
         })
@@ -40,6 +61,8 @@ const App = () =>{
         <form onSubmit={onSubmit}>
             <div>
                 <h1> Hello {fullName.fname} {fullName.lname} </h1>
+                <p>{fullName.email}</p>
+                <p>{fullName.phone}</p>
                 <input type="text" 
                     placeholder="Enter Your Name"
                     name="fName" 
@@ -52,13 +75,24 @@ const App = () =>{
                     onChange={inputValue}
                     value={fullName.lname}
                 />
+                <input type="email" 
+                    placeholder="Enter Your email"
+                    name="email" 
+                    onChange={inputValue}
+                    value={fullName.email}
+                    // autoComplete="off"
+                />
+                <input type="number" 
+                    placeholder="Enter Your Phone Number"
+                    name="phone" 
+                    onChange={inputValue}
+                    value={fullName.phone}
+                />
                 <button type="submit"> Submit</button>
             </div>
         </form>
     </div>
     </>
-
     );
 }
-
 export default App;
