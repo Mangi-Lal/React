140fc295a6ba74013cdacb7309da5cb063cdf58a
Mangi Lal <mangilal1036@gmail.com>
2023-10-15 11:55:08 +0530

    useEffect hook Challenge

--- react_v1/src/App.jsx
+++ react_v1/src/App.jsx
@@ -2,34 +2,16 @@ import React, { useEffect, useState } from "react";
 
 const App = () =>{
     const [num,setNum] = useState(0);
-    const [num2,setNum2] = useState(0);
     useEffect(
-        //everytime we click any button or refresh the page this function is called.
-        // () =>{alert('clicked');}
-        //empty array ([]) means jb first time page render hoga only tbhi ye function call hoga..
-        // () =>{alert('clicked');},[]
-        //function is called only when page refreshed and button associated with 'num' is pressed..
-        () =>{alert('clicked');},[num]
+        () =>{document.title = `You clicked ${num} times`}
     )
-
     return (
     <>
     <button onClick={(() =>{
         setNum(num+1);
     }
-    //If we not use useEffect then only alert vala function is called
-    // () =>{
-    //     alert('Clicked');
-    // }
     )}>
     Click {num}
-    </button>
-    <br/>
-    <button onClick={(() =>{
-        setNum2(num2+1);
-    }
-    )}>
-    Click {num2}
     </button>
     </>
     );
