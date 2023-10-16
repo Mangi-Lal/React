01cf9b0448ec5bf8b7cf1dff8d192687901067c4
Mangi Lal <mangilal1036@gmail.com>
2023-10-16 14:29:42 +0530

    useLocation hook in react router

--- react_v2/src/User.jsx
+++ react_v2/src/User.jsx
@@ -1,6 +1,6 @@
 //User.jsx
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 // Without hooks
 // const User = ({match}) =>{
@@ -10,9 +10,19 @@ import { useParams } from "react-router-dom";
 // With hooks
 
 const User = () =>{
+    const location = useLocation();
+    // console.log(location);
     //fname,lname names should match with parameters passed in path using Route...
     const {fname,lname} = useParams();
-    return <h1>This is {fname} {lname} Page</h1>
+    return (
+        <>
+        <h1>This is {fname} {lname} Page</h1>
+        <p>Your current location is {location.pathname}</p>
+        {location.pathname === `/user/mangi/lal`?
+        <button onClick={()=>{alert('Got it')}}>Click me</button>:null
+        }
+        </>
+    );
 }
 
 export default User;
