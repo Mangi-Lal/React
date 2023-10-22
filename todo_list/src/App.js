d856f0ecda32f7cc7ae3222b7409d1c4f9f56384
Mangi Lal <mangilal1036@gmail.com>
2023-10-22 22:43:05 +0530

    Done todo list

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TodoList from './TodoList';
 
 const App = () => {
+    const [input, setInput] = useState("");
+    const [itemlist, setItemlist] = useState([]);
+
+    const inputData = (event) =>{
+        setInput(event.target.value);
+    }
+    const listOfItems = () =>{
+        setItemlist((oldItems)=>{
+            return [...oldItems, input];
+        });
+        setInput("");
+    }
+    const deleteItems = (id) => {
+        // console.log(deleted);
+        setItemlist((oldItems) =>{
+            return oldItems.filter((arrElem,index)=>{
+                return index !== id;
+             });
+        });
+    }
+
   return (
-    <h1>Hiiii</h1>
+    <>
+    <div className='main_div'>
+        <div className='center_div'>
+            <br />
+            <h1>ToDo List</h1>
+            <br />
+
+            <input type="text" 
+            placeholder='Add your items'
+            value={input} 
+            onChange={inputData}/>
+
+            <button onClick={listOfItems}>+</button>
+            <ol>
+                {itemlist.map((itemval,index) =>{
+                    return <TodoList key = {index} 
+                    id = {index} 
+                    text = {itemval}
+                    onSelect = {deleteItems}
+                    />;
+                })}
+            </ol>
+
+        </div>
+    </div>
+    </>
   )
 }
 
